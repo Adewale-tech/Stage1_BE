d9715870f57feaab816f9fd4ce6c74f32423e1b7
Adewale-tech <waliyullahadewale30@gmail.com>
2025-10-23 11:48:08 +0100

    Refactor data file path to be relative to the project directory

--- BE-Stage1/main.py
+++ BE-Stage1/main.py
@@ -7,7 +7,9 @@ import os
 
 app = FastAPI()
 
-DATA_FILE = "data.json"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_FILE = os.path.join(BASE_DIR, "data.json")
+
 
 # Load or initialize data file
 if not os.path.exists(DATA_FILE):
